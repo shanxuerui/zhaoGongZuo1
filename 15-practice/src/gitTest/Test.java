f43b998801285b3fe1f17fb7aac12447b72e882f
shanxuerui <53455011+shanxuerui@users.noreply.github.com>
2020-02-29 21:30:24 +0800

    Update Test.java

--- 15-practice/src/gitTest/Test.java
+++ 15-practice/src/gitTest/Test.java
@@ -6,7 +6,7 @@ package gitTest;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("3");
-             System.out.println("4");
+        System.out.println("四猪");
+             System.out.println("二猪");
     }
 }
