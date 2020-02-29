faca72f0728e346fe29c54dec3ea0ca37eaeb4b9
bj <bj@236.com>
2020-02-29 21:33:26 +0800

    15

--- 15-practice/src/gitTest/Test.java
+++ 15-practice/src/gitTest/Test.java
@@ -6,7 +6,7 @@ package gitTest;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("四猪");
-             System.out.println("二猪");
+        System.out.println("四狗");
+             System.out.println("二狗");
     }
 }
