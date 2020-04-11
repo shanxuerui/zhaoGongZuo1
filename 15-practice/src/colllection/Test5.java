f75f6671d7e43b8698bea6e3d2aa9d783acfeb18
shanxuerui <53455011+shanxuerui@users.noreply.github.com>
2020-04-11 20:33:08 +0800

    Update Test5.java

--- 15-practice/src/colllection/Test5.java
+++ 15-practice/src/colllection/Test5.java
@@ -6,7 +6,7 @@ package colllection;
  */
 public class Test5 {
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println(2);
         System.out.println(2);
         System.out.println(3);
  System.out.println(3);
