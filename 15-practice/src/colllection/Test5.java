3c2ce367563431bf13c3801784510ec58737dd02
jshanxuerui <764795838@qq.com>
2020-04-11 20:28:51 +0800

    test5

---
+++ 15-practice/src/colllection/Test5.java
@@ -0,0 +1,13 @@
+package colllection;
+
+/**
+ * Author:单雪瑞
+ * 2020/4/11 20:28
+ */
+public class Test5 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
