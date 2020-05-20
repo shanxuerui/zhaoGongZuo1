b0a39f6827ae5b304397c6a04a2eb2fb60d819a8
jshanxuerui <764795838@qq.com>
2020-05-20 10:50:37 +0800

    test

---
+++ 20-worktest/src/Test01/Test01.java
@@ -0,0 +1,15 @@
+package Test01;
+
+/**
+ * @ClassName Test01
+ * @Author:单雪瑞
+ * @Date 2020/5/12 11:19
+ */
+public class Test01 {
+    public static void main(String[] args) {
+
+
+        System.out.println("111");
+        System.out.println("test");
+    }
+}
